192085e8b56c347306f35b45621f23a197b981ca
Tech-A32 <Tech-A32@Tech-A32s-MacBook-Air.local>
2017-08-30 13:16:45 +0100

    checking array in hash

--- spec/weather_map_spec.rb
+++ spec/weather_map_spec.rb
@@ -8,12 +8,21 @@ describe OpenWeatherMap do
 	end
 
 
-
-	it "the name key should not have the value of Japan" do
-		expect(@weatherData['name']).to eq "Japan"  
-	end 
+	 it "the name key should not have the value of Japan" do
+		 expect(@weatherData['name']).to eq "Japan"  
+	 end 
 
 	it "the name key should have the value of a string called London" do 
 		expect(@weatherData['name']).to eq "London"
 	end 
+
+	it "the coord should have a key called lon within it" do 
+		@weatherData["coord"].each do |key,value|
+			expect("lon").to eq "lon"
+		end
+	end
+
+	it "the weather key should have an array" do 
+		expect(@weatherData['weather'].class).to eq Array 
+	end
 end
