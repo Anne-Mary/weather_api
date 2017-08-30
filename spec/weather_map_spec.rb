b9a0901c7e9614f6b1f33b6b7d75316ec209f391
Tech-A32 <Tech-A32@Tech-A32s-MacBook-Air.local>
2017-08-30 12:18:47 +0100

    2 tests running

--- spec/weather_map_spec.rb
+++ spec/weather_map_spec.rb
@@ -3,13 +3,17 @@ require_relative "../weather.rb"
 describe OpenWeatherMap do 
 
 	before(:each) do 
-		 @weather = OpenWeatherMap.new
-		@x = @weather.LondonWeather 
+		@weather = OpenWeatherMap.new
+		@weatherData = @weather.LondonWeather 
 	end
 
 
 
-	it "should have show results" do 
-		expect(@x['name']).to eq "Japan"
+	it "the name key should not have the value of Japan" do
+		expect(@weatherData['name']).to eq "Japan"  
+	end 
+
+	it "the name key should have the value of a string called London" do 
+		expect(@weatherData['name']).to eq "London"
 	end 
 end
